a274a35f48d9112c47fa4c03e423acb8d572d73a
Sylvia <sylviakieha@gmail.com>
2016-03-09 18:59:34 +0300

    mapping user relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ActiveRecord::Base
+  belongs_to :referrer, class_name: 'User'
+  belongs_to :referral, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  validates :phone_number, uniqueness: true, presence: true
+
+  has_many :referral_relationships, class_name: 'Relationship', foreign_key: 'referrer_id'
+  has_many :referrals, through: referral_relationships
+
+  has_one :referrer_relationship, class_name: 'Relationship', foreign_key: 'referral_id'
+  has_one :referrer, through: referrer_relationship
 end

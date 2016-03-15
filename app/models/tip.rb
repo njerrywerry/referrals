3584d205be98a0676e7cabeed468c0684a1ee47f
Sylvia <sylviakieha@gmail.com>
2016-03-15 21:39:43 +0300

    associating users to tips

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,3 +1,5 @@
 class Tip < ActiveRecord::Base
   default_value_for :commission_rate, 0.03
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   has_one :referrer_relationship, class_name: 'Relationship', foreign_key: 'referral_id'
   has_one :referrer, through: :referrer_relationship
 
+  has_many :tips
+
   def refer user
     Relationship.create referrer_id: self.id, referral_id: user.id
   end
